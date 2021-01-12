f467f3cf7d14ab384d69b92bc23730f152da66cd
Top-Vladon <77295868+Top-Vladon@users.noreply.github.com>
2021-01-12 16:08:36 +0300

    Update bot.py.py

--- bot.py.py
+++ bot.py.py
@@ -12,4 +12,4 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hello!!!")
 
-Bot.run("Nzk3ODU2NjY5Nzk4MjM2MTYx.X_sj7Q.1Q3vSGqQtAMpfLyai9SD0cLTE6M")
+Bot.run("Nzk3ODU2NjY5Nzk4MjM2MTYx.X_sj7Q.zMj7yTXSnkVxX-bdzPeob8-T0-c")
